0523ac27656f4d0d174199f85da16bb04037e928
SulmanAli2 <38355522+SulmanAli2@users.noreply.github.com>
2020-07-09 16:53:56 +0200

    quiz.js wrongA

--- js/quiz.js
+++ js/quiz.js
@@ -1,52 +1,50 @@
-// code by webdevtrick (https://webdevtrick.com)
+
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
     this.questionIndex = 0;
-    
+
 }
 
-
-Quiz.prototype.getQuestionIndex = function() {
+Quiz.prototype.getQuestionIndex = function () {
     return this.questions[this.questionIndex];
 }
 
-Quiz.prototype.guess = function(answer) {
-    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+Quiz.prototype.guess = function (answer) {
+    if (this.getQuestionIndex().isCorrectAnswer(answer)) {
         this.score++;
     }
     this.questionIndex++;
 }
 
-Quiz.prototype.isEnded = function() {
+Quiz.prototype.isEnded = function () {
     return this.questionIndex === this.questions.length;
 }
-
 
 function Question(text, choices, answer) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
-}
 
-Question.prototype.isCorrectAnswer = function(choice) {
+}
+Question.prototype.isCorrectAnswer = function (choice) {
+    userAnswers.push(choice);
     return this.answer === choice;
 }
 
-
 function populate() {
-    if(quiz.isEnded()) {
+    if (quiz.isEnded()) {
         showScores();
-    }
-    else {
-        // show question
+        for (var i = 0; i < questions.length; i++) {
+            isWrong(questions[i], i);
+        }
+
+    } else {
         var element = document.getElementById("question");
         element.innerHTML = quiz.getQuestionIndex().text;
-        
 
-        // show options
         var choices = quiz.getQuestionIndex().choices;
-        for(var i = 0; i < choices.length; i++) {
+        for (var i = 0; i < choices.length; i++) {
             var element = document.getElementById("choice" + i);
             element.innerHTML = choices[i];
             guess("btn" + i, choices[i]);
@@ -58,17 +56,38 @@ function populate() {
 
 function guess(id, guess) {
     var button = document.getElementById(id);
-    button.onclick = function() {
+    button.onclick = function () {
         quiz.guess(guess);
         populate();
     }
 };
 
+var userAnswers = [];
 
+//Show Wrong Answers
+function isWrong(question,index) {
+
+    var answer = userAnswers[index];
+    alert("User answer for question: " + (question.text) + (index+1) + " : " + userAnswers[index]);
+    if(answer == question["answer"]){
+        alert("Correct");
+    }
+    else{
+        //incorrectA.push(index);
+        alert('Sorry. The correct answer is: ' + question["answer"]);
+    }
+  
+    if(quiz.score>=5){
+        alert("YOU WON, CONGRATULATIONS!");
+    }
+    if(quiz.score<5){
+        alert("YOU LOSED, TRY AGAIN");
+        quiz.score = 0;
+    }
+
+}
 function showProgress() {
-    
     var currentQuestionNumber = quiz.questionIndex + 1;
-    
     var element = document.getElementById("progress");
     element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
 };
@@ -80,20 +99,21 @@ function showScores() {
     element.innerHTML = gameOverHTML;
 };
 
-// create questions here
 var questions = [
-    new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
-    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
-    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
-    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
-    new Question("What will happen with the following code?", ["Oracle", "Microsoft", "Netscape", "Other"], "Netscape")
+    new Question("Hyper Text Markup Language Stand For? ", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
+    new Question("Which language is used for styling web pages? ", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("Which is not a JavaScript Framework? ", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
+    new Question("Which is used for Connect To Database? ", ["PHP", "HTML", "JS", "All"], "PHP"),
+    new Question("What will happen with the following code? ", ["Oracle", "Microsoft", "Netscape", "Other"], "Netscape")
    
     
     
 ];
 
-// create quiz
+//Create New Quiz
 var quiz = new Quiz(questions);
-
-// display quiz
+//show Quiz
 populate();
+
+
+
